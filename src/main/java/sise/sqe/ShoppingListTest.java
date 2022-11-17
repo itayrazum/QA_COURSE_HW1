5aaff55d74736544bf2a1618aefa336398cc201f
itayrazum <itayrazum@gmail.com>
2022-11-17 12:19:55 +0200

    first commit

--- src/main/java/sise/sqe/ShoppingListTest.java
+++ src/main/java/sise/sqe/ShoppingListTest.java
@@ -1,7 +1,19 @@
 package sise.sqe;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShoppingListTest {
+    private ShoppingList shoppingList;
+
+    @BeforeAll
+    public void Init(){
+        shoppingList = Mockito.mock(ShoppingList.class);
+    }
+
 
 }
